d9e206651cb603e9098ef2a8e743d5cdf233ab29
Alanis <nanissotelo3@gmail.com>
2025-06-16 23:04:30 -0600

    Actualización de funcionalidades y correcciones en registro

--- main/static/main/auth.js
+++ main/static/main/auth.js
@@ -212,7 +212,7 @@ function handleRegisterSubmit(e) {
 
     setTimeout(() => {
       // Redirect to login page
-      window.location.href = "login.html"
+      window.location.href = "/inicioSesion/"
     }, 2000)
   }, 2500)
 }
